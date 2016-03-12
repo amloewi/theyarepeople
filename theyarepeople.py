457cd601005f4d15219a84d7fd2971a166531eab
Alexander Loewi <amloewi@gmail.com>
2016-03-12 01:30:50 -0500

    Now PORT is determined automatically (necessary)
    Signed-off-by: Alexander Loewi <amloewi@gmail.com>

--- theyarepeople.py
+++ theyarepeople.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +9,7 @@ def hello():
     return render_template('theyarepeople.html')
 
 if __name__ == "__main__":
-    app.run()
+    # Thanks, stx (again)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
